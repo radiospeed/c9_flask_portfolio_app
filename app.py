79998c2d97ab4ad7fcdb1dcab1fbb974fa6c3d18
radiospeed <51825016+radiospeed@users.noreply.github.com>
2019-06-15 01:08:28 +1000

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def add_numbers_post():
   	      print(request.form['text'].split())
   	      total = 0
   	      try:
+		strofnums = request.form['text'].split()
 		for str_num in request.form['text'].split():
   	      		total *= int(str_num)
   	      	return render_template('add_numbers.html', result=str(total))
